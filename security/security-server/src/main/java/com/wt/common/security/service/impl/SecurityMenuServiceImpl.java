b4292ccf8b9b07583c049badf3e4a25631a6f515
lichking2019 <lichking2019@aliyun.com>
2019-04-15 16:25:11 +0800

    调整菜单服务

--- security/security-server/src/main/java/com/wt/common/security/service/impl/SecurityMenuServiceImpl.java
+++ security/security-server/src/main/java/com/wt/common/security/service/impl/SecurityMenuServiceImpl.java
@@ -2,10 +2,13 @@ package com.wt.common.security.service.impl;
 
 import com.wt.common.security.dao.SecurityMenuMapperSupport;
 import com.wt.common.security.domain.SecurityMenu;
+import com.wt.common.security.domain.SecurityUser;
 import com.wt.common.security.service.SecurityMenuService;
+import com.wt.common.security.service.SecurityUserService;
 import com.wt.master.core.base.support.ServiceSupport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * 菜单服务
